547fd4b462924d35a2505e361ed82529dd9b4a01
Earlinn Drow <earlinndrow@gmail.com>
2020-04-02 01:19:57 +0300

    Flask-Migrations и регистрация пользователей

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, flash, render_template, redirect, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
-from webapp.user.forms import LoginForm
+from webapp import db
+from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
@@ -18,7 +19,7 @@ def login():
 def process_login():
     form = LoginForm()
 
-    if form.validate_on_submit():
+    if form.validate_on_submit(): # если форма валидна (при ее проверке не произошло ошибок)
         user = User.query.filter(User.username == form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
@@ -33,3 +34,24 @@ def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('news.index'))
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    title = 'Регистрация'
+    form = RegistrationForm()
+    return render_template('user/registration.html', page_title=title, form=form)
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # если форма валидна
+        new_user = User(username=form.username.data, email=form.email.data, role='user') # вот так надо и по словарям искать на deja vu words
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались') # тоже использовать для deja vu words при поиске слов
+        return redirect(url_for('user.login'))
+    flash('Пожалуйста, исправьте ошибки в форме регистрации')
+    return redirect(url_for('user.register'))
