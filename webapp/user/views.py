28e5d66f184fa1faf5ca4efa22163b39e305a1ee
Earlinn Drow <earlinndrow@gmail.com>
2020-05-08 00:34:11 +0300

    Добавлены комментарии, но есть баг в файле utils

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,36 +4,41 @@ from flask_login import current_user, login_required, login_user, logout_user
 from webapp import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
+
 
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
     title = 'Авторизация'
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
+
 
 @blueprint.route('/process-login', methods=['POST'])
 def process_login():
     form = LoginForm()
 
-    if form.validate_on_submit(): # если форма валидна (при ее проверке не произошло ошибок)
+    if form.validate_on_submit():
         user = User.query.filter(User.username == form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
 
     flash('Неправильные имя или пароль') 
     return redirect(url_for('user.login'))
+
 
 @blueprint.route('/logout')
 def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('news.index'))
+
 
 @blueprint.route('/register')
 def register():
@@ -43,18 +48,20 @@ def register():
     form = RegistrationForm()
     return render_template('user/registration.html', page_title=title, form=form)
 
+
 @blueprint.route('/process-reg', methods=['POST'])
 def process_reg():
     form = RegistrationForm()
-    if form.validate_on_submit(): # если форма валидна
-        new_user = User(username=form.username.data, email=form.email.data, role='user') # вот так надо и по словарям искать на deja vu words
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, role='user')
         new_user.set_password(form.password.data)
         db.session.add(new_user)
         db.session.commit()
-        flash('Вы успешно зарегистрировались') # тоже использовать для deja vu words при поиске слов
+        flash('Вы успешно зарегистрировались')
         return redirect(url_for('user.login'))
     else:
         for field, errors in form.errors.items():
             for error in errors:
                 flash(f'Ошибка в поле {getattr(form, field).label.text}: {error}')
+    
         return redirect(url_for('user.register'))
